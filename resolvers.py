3bdd3bd3e7baa52d8702b4c64531c4dd520c91dd
raagapranitha <raagapraneeta@gmail.com>
2020-02-27 15:21:14 -0800

    Added student to class

--- resolvers.py
+++ resolvers.py
@@ -2,19 +2,21 @@ import json
 import time
 import math
 
-students = dict({"1": "Sachin Tendulkar", "2": "Dravid"})
-classs=dict({})
+students = {}
+classs= {}
 
 def student_with_id(_,info, studentId):
     new_student={}
     new_student['studentId']=studentId
     new_student['name']=students[studentId]
+    #students.append(new_student)
     return new_student
 
 def class_with_id(_,info,classId):
     new_class={}
     new_class['classId']=classId
     new_class['name']=classs[classId]
+    #classs.append(new_class)
     return new_class
 
 def resolver_createStudent(_,info, studentName):
@@ -29,25 +31,29 @@ def resolver_createClass(_,info,className):
     new_class={}
     new_class['classId']=get_new_id()
     new_class['name']=className
-    classs[new_class['classId']]=className
+    new_class['students']=[]
+    classs[new_class['classId']]=new_class
     return new_class
 
 def resolver_add_student_to_class(_,info,classId,studentId):
-    index=-1
-    curr_class={}
-    for i in range(0,len(students)):
-        if (students[i]).get('studentId')==studentId:
-            index=i
-            break
-    if index==-1:
-        print('No student with the id')
-    else:   
-        for i in range(0,len(classs)):
-            if (classs[i]).get('classId')==classId:
-                (classs[i]).get('students').append(students[index])
-                curr_class=classs[i]
-                break       
-    return  curr_class         
+    curr_class = classs[classId]
+    (curr_class['students']).append(studentId)
+    return curr_class
+    # index=-1
+    # curr_class={}
+    # for i in range(0,len(students)):
+    #     if (students[i]).get('studentId')==studentId:
+    #         index=i
+    #         break
+    # if index==-1:
+    #     print('No student with the id')
+    # else:   
+    #     for i in range(0,len(classs)):
+    #         if (classs[i]).get('classId')==classId:
+    #             (classs[i]).get('students').append(students[index])
+    #             curr_class=classs[i]
+    #             break       
+    # return  curr_class         
            
 
 def get_new_id():
